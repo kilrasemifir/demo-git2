1f2e617ca65a00a6316e4657e386631e52a47d0b
Killian Raoux <killian@semifir.com>
2022-07-19 13:46:12 +0200

    feat: ajout de la fonction fibonacci

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print("Hello world")
+def fibonacci(n):
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fibonacci(n-1) + fibonacci(n-2)
 
+def main():
+  print("Lancement du programme")
+  fibonacci(int(input("Entrez un nombre : ")))
 
-print("Ajout de La FEATURE!!")
+if __name__ == "__main__":
+  main()
